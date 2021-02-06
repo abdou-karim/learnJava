c6a4c119d150d85aae31e8974c79e394c7902bb6
abdou-karim <abdoukarimsidibe1@gmail.com>
2021-02-06 11:54:14 +0000

    condition Si.....Sinon

--- src/firstCours/Main.java
+++ src/firstCours/Main.java
@@ -6,11 +6,13 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Veuillez saisir un entier");
+        System.out.println("Veuillez saisir un nombre reel");
         int i = sc.nextInt();
-        sc.nextLine();
-        System.out.println("Veuillez saisir un mot: ");
-        String str = sc.nextLine();
-        System.out.println("Vous avez saisis " + i +" et " + str);
+        if(i<0){
+            System.out.println("Vous avez saisis un nombre negatif");
+        }else {
+            System.out.println("Vous avez saisis un nombre positif");
+        }
+
     }
 }
