6e99786a28b4ba23682e419988f1b17e7c58e528
abdou-karim <abdoukarimsidibe1@gmail.com>
2021-02-06 10:36:21 +0000

    normal resultat

--- src/firstCours/Main.java
+++ src/firstCours/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
    double nbr1=10,nbr2=3;
-   int resultat= (int) (nbr1/nbr2);
+   double resultat=  (nbr1/nbr2);
    System.out.println(resultat);
     }
 }
