9c84bbf2d42fe6883beb35d5300083054bec3311
abdou-karim <abdoukarimsidibe1@gmail.com>
2021-02-06 11:45:15 +0000

    recuperer plusieur saisis

--- src/firstCours/Main.java
+++ src/firstCours/Main.java
@@ -6,8 +6,11 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Veuillez saisir un entier");
+        int i = sc.nextInt();
+        sc.nextLine();
         System.out.println("Veuillez saisir un mot: ");
         String str = sc.nextLine();
-        System.out.println("Vous avez saisis " + str);
+        System.out.println("Vous avez saisis " + i +" et " + str);
     }
 }
